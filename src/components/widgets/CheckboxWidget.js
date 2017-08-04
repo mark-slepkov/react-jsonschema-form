31de069cff02e88baa5cd98b69a1572abbf4ee9e
mark-slepkov <1qwer432>
2017-08-04 19:13:21 +0300

    adaptation for react native Checkbox

--- src/components/widgets/CheckboxWidget.js
+++ src/components/widgets/CheckboxWidget.js
@@ -1,56 +1,58 @@
 import React from "react";
 import PropTypes from "prop-types";
 import DescriptionField from "../fields/DescriptionField.js";
+import {View, Text} from 'react-native';
+import {Checkbox} from 'react-native-material-design';
+
 
 function CheckboxWidget(props) {
-  const {
-    schema,
-    id,
-    value,
-    required,
-    disabled,
-    readonly,
-    label,
-    autofocus,
-    onChange,
-  } = props;
-  return (
-    <div className={`checkbox ${disabled || readonly ? "disabled" : ""}`}>
-      {schema.description &&
-        <DescriptionField description={schema.description} />}
-      <label>
-        <input
-          type="checkbox"
-          id={id}
-          checked={typeof value === "undefined" ? false : value}
-          required={required}
-          disabled={disabled || readonly}
-          autoFocus={autofocus}
-          onChange={event => onChange(event.target.checked)}
-        />
-        <span>
-          {label}
-        </span>
-      </label>
-    </div>
-  );
+    const {
+        schema,
+        id,
+        value,
+        required,
+        disabled,
+        readonly,
+        label,
+        autofocus,
+        onChange,
+    } = props;
+    return (
+        <View>
+            {schema.description &&
+            <DescriptionField description={schema.description}/>}
+            <View>
+                <Checkbox
+                    id={id}
+                    checked={typeof value === "undefined" ? false : value}
+                    required={required}
+                    disabled={disabled || readonly}
+                    autoFocus={autofocus}
+                    onChange={event => onChange(event.target.checked)}
+                />
+                <Text>
+                    {label}
+                </Text>
+            </View>
+        </View>
+    );
 }
 
 CheckboxWidget.defaultProps = {
-  autofocus: false,
+    autofocus: false,
 };
 
 if (process.env.NODE_ENV !== "production") {
-  CheckboxWidget.propTypes = {
-    schema: PropTypes.object.isRequired,
-    id: PropTypes.string.isRequired,
-    value: PropTypes.bool,
-    required: PropTypes.bool,
-    disabled: PropTypes.bool,
-    readonly: PropTypes.bool,
-    autofocus: PropTypes.bool,
-    onChange: PropTypes.func,
-  };
+    CheckboxWidget.propTypes = {
+        schema: PropTypes.object.isRequired,
+        id: PropTypes.string.isRequired,
+        value: PropTypes.bool,
+        required: PropTypes.bool,
+        disabled: PropTypes.bool,
+        readonly: PropTypes.bool,
+        autofocus: PropTypes.bool,
+        onChange: PropTypes.func,
+    };
 }
 
 export default CheckboxWidget;
